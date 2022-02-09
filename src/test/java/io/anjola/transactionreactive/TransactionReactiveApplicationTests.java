ee79a154236c5c86a5d32db25319f94671e98136
Sanni Lateef Anjola <sanni.anjola@gmail.com>
2022-02-09 15:00:19 +0100

    Project setup and api documentation integration

---
+++ src/test/java/io/anjola/transactionreactive/TransactionReactiveApplicationTests.java
@@ -0,0 +1,13 @@
+package io.anjola.transactionreactive;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TransactionReactiveApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
